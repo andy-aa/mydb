08d1d3906f5bd5356e1935d603d29a5f09f65770
andy-aa <46691193+andy-aa@users.noreply.github.com>
2019-09-14 20:18:47 +0300

    Update DbEntityPageTrait.php

--- src/DbEntityPageTrait.php
+++ src/DbEntityPageTrait.php
@@ -6,7 +6,7 @@ trait DbEntityPageTrait
 {
     protected $pageSize;
 
-    public function setPageSize(int $size): object
+    public function setPageSize(int $size)
     {
         $this->pageSize = $size;
         return $this;
@@ -17,7 +17,7 @@ trait DbEntityPageTrait
         return $this->setPageLimit($page)->get();
     }
 
-    protected function setPageLimit(int $page): object
+    protected function setPageLimit(int $page)
     {
         if (!is_null($page)) {
             $this->queryCustom['LIMIT'] = (($page - 1) * $this->pageSize) . " , $this->pageSize";
@@ -27,6 +27,6 @@ trait DbEntityPageTrait
 
     public function pageCount(): int
     {
-        return (int) ceil($this->rowCount() / $this->pageSize);
+        return (int)ceil($this->rowCount() / $this->pageSize);
     }
 }
