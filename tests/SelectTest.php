d75d9269f2c21c7393fb8f8b5195fd050546c73e
andy-aa <46691193+andy-aa@users.noreply.github.com>
2019-09-15 14:23:42 +0300

    unit tests

---
+++ tests/SelectTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * from phpunit.xml
+ * @var string $mysql_db
+ * @var string $mysql_host
+ * @var string $mysql_user
+ * @var string $mysql_pass
+ */
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use TexLab\LightDB\DbEntity;
+
+class SelectTest extends TestCase
+{
+
+    function testSelect()
+    {
+        global $mysql_db,
+               $mysql_host,
+               $mysql_user,
+               $mysql_pass,
+               $mysql_test_table;
+
+        $table = new DbEntity($mysql_test_table,
+            new mysqli(
+                $mysql_host,
+                $mysql_user,
+                $mysql_pass,
+                $mysql_db
+            )
+        );
+
+        $this->assertIsArray(
+            $table->get(),
+            'Is not Object'
+        );
+
+    }
+
+}
