7d35491f2bcb22540e3be3230bb1648c5c653024
andy-aa <46691193+andy-aa@users.noreply.github.com>
2020-07-28 11:11:04 +0300

    test3

--- src/Table.php
+++ src/Table.php
@@ -37,8 +37,7 @@ class Table extends Runner implements CRUDInterface
 
             $bufWHERE = $this->queryCustom['WHERE'] ?? null;
 
-            $this->queryCustom['WHERE'] = (empty($bufWHERE) ? '' : 'AND ') . $this->createWhereCondition($conditions);
-//            $this->queryCustom['WHERE'] = $this->createWhereCondition($conditions);
+            $this->queryCustom['WHERE'] = (empty($bufWHERE) ? '' : "$bufWHERE AND ") . $this->createWhereCondition($conditions);
 
             $result = $this->runSQL($this->getSQL());
 
